dfd10ed97a1cdc936f5e22c5a653e4e0045c0737
Sean Dague <sean@dague.net>
2014-06-16 19:47:18 -0400

    updated for Ubuntu 14.04

--- imaway.py
+++ imaway.py
@@ -6,30 +6,31 @@ import gobject
 import subprocess
 
 
-def go_screensaver(active):
-    if active:
-        print "Screen saver turned on"
-        subprocess.call("pactl set-sink-mute 1 1", shell=True)
-        subprocess.call("xchat -e -c 'AWAY'", shell=True)
-    else:
-        subprocess.call("pactl set-sink-mute 1 0", shell=True)
-        subprocess.call("xchat -e -c 'BACK'", shell=True)
-        print "Screen saver deactivated"
+def lock():
+    print "Screen saver turned on"
+    subprocess.call("pactl set-sink-mute 0 1", shell=True)
+    subprocess.call("xchat -e -c 'AWAY'", shell=True)
+
+
+def unlock():
+    subprocess.call("pactl set-sink-mute 0 0", shell=True)
+    subprocess.call("xchat -e -c 'BACK'", shell=True)
+    print "Screen saver deactivated"
 
 
 def main(args):
     bus = dbus.SessionBus()
 
-    # # figure out what the screensaver is up to right now
-    # saver_proxy = bus.get_object('org.gnome.ScreenSaver',
-    #                              '/org/gnome/ScreenSaver')
-    # minder.screensaver_active_state_changed(saver_proxy.GetActive())
-
     # listen for changes in screensaver activity
     bus.add_signal_receiver(
-        go_screensaver,
-        'ActiveChanged',
-        'org.gnome.ScreenSaver')
+        lock,
+        'Locked',
+        dbus_interface='com.canonical.Unity.Session')
+
+    bus.add_signal_receiver(
+        unlock,
+        'Unlocked',
+        dbus_interface='com.canonical.Unity.Session')
 
     mainLoop = gobject.MainLoop()
     mainLoop.run()
