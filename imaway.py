4d4f8d4262ab228572f81c6b84880666f0bbd078
Sean Dague <sean@dague.net>
2014-10-10 10:36:13 -0400

    updates for robustness
    Comb through the entire pulse audio device list for possible sinks, as
    in Ubuntu the same audio device seems to jump around after kernel
    updates, making the mute / unmute stop working.
    
    Also, don't do the xchat calls if xchat is not running. This prevents
    us from spawning useless xchat windows in the event that we've turned
    off xchat because we're on paternity leave, but are still using the
    computer.

--- imaway.py
+++ imaway.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python
 
+import subprocess
+
 import dbus
 import dbus.glib
 import gobject
-import subprocess
+import psutil
+
+
+MUTE_CMD = "pactl set-sink-mute %s %s"
+
+
+def is_xchat_running():
+    return "xchat" in [psutil.Process(i).name for i in psutil.get_pid_list()]
+
+
+def find_sinks():
+    p = subprocess.Popen("pactl list | grep 'Sink #'",
+                         stdout=subprocess.PIPE, shell=True)
+    output = p.communicate()[0]
+    lines = output.split('\n')
+    return [int(x.split('Sink #')[1]) for x in lines if x]
+
+
+def mute(mute=1):
+    sinks = find_sinks()
+    for sink in sinks:
+        print "Sending %s" % (MUTE_CMD % (sink, mute))
+        subprocess.call(MUTE_CMD % (sink, mute), shell=True)
 
 
 def lock():
     print "Screen saver turned on"
-    subprocess.call("pactl set-sink-mute 0 1", shell=True)
-    subprocess.call("xchat -e -c 'AWAY'", shell=True)
+    mute()
+    if is_xchat_running():
+        subprocess.call("xchat -e -c 'AWAY'", shell=True)
 
 
 def unlock():
-    subprocess.call("pactl set-sink-mute 0 0", shell=True)
-    subprocess.call("xchat -e -c 'BACK'", shell=True)
+    mute(mute=0)
+    if is_xchat_running():
+        subprocess.call("xchat -e -c 'BACK'", shell=True)
     print "Screen saver deactivated"
 
 
